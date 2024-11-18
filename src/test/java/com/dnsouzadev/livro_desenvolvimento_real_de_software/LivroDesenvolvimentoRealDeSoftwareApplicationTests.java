79ee87570c962bc730b8179d3e02851e8588d9a9
Daniel Souza <workdndsza@gmail.com>
2024-11-18 15:43:36 -0300

    iniciando o livro

---
+++ src/test/java/com/dnsouzadev/livro_desenvolvimento_real_de_software/LivroDesenvolvimentoRealDeSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.livro_desenvolvimento_real_de_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivroDesenvolvimentoRealDeSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
